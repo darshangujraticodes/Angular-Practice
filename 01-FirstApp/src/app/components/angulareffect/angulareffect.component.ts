e0f5be763167f1c7a1c13f2c0962b136ff7b65b5
darshangujraticodes <darshangujrati5@gmail.com>
2025-05-05 15:37:13 +0530

    angular dynamic styles

--- 01-FirstApp/src/app/components/angulareffect/angulareffect.component.ts
+++ 01-FirstApp/src/app/components/angulareffect/angulareffect.component.ts
@@ -10,6 +10,12 @@ export class AngulareffectComponent {
   counter = signal(1);
   displayEvenCounter = false;
 
+  bgColor = 'black';
+  textColor = 'white';
+  border = '2px solid black';
+
+  darkMode = true;
+
   constructor() {
     effect(() => {
       if (this.counter() % 2 === 0) {
@@ -26,5 +32,16 @@ export class AngulareffectComponent {
 
   incrementData() {
     this.counter.set(this.counter() + 1);
+  }
+
+  switchThemeMode() {
+    this.darkMode = !this.darkMode;
+    if (this.darkMode) {
+      this.bgColor = 'black';
+      this.textColor = 'white';
+    } else {
+      this.bgColor = 'white';
+      this.textColor = 'black';
+    }
   }
 }
--- 01-FirstApp/src/app/components/angulartodo/angulartodo.component.ts
+++ 01-FirstApp/src/app/components/angulartodo/angulartodo.component.ts
@@ -16,6 +16,78 @@ type taskType = {
 export class AngulartodoComponent {
   taskName = '';
   taskStatus = '';
+  editMode = false;
+  editDataId = 0;
+  error: string[] = [];
 
   taskList: taskType[] = [];
+
+  addTodoTask() {
+    // console.log(this.taskName);
+    // console.log(this.taskStatus);
+
+    if (this.taskName == '') {
+      this.error.push('taskNameError');
+    } else if (this.taskStatus == '') {
+      this.error.pop();
+      this.error.push('taskStatusError');
+    } else {
+      this.taskList.push({
+        id: Date.now(),
+        task: this.taskName,
+        status: this.taskStatus,
+      });
+
+      console.log(this.taskList);
+
+      this.taskName = '';
+      this.taskStatus = '';
+      this.error = [];
+    }
+  }
+
+  resetTodo() {
+    this.taskName = '';
+    this.taskStatus = '';
+    this.editMode = false;
+  }
+
+  taskReset() {
+    this.taskList = [];
+    this.editMode = false;
+    this.editDataId = 0;
+  }
+
+  deleteTask(taskId: number) {
+    this.taskList = this.taskList.filter((item) => item.id != taskId);
+  }
+
+  updateTask(taskId: number) {
+    const updateTaskData = this.taskList.map((item) => {
+      if (item.id === taskId) {
+        this.editDataId = item.id;
+        console.log(item.id, item.task, item.status);
+        this.taskName = item.task;
+        this.taskStatus = item.status;
+      }
+    });
+
+    this.editMode = true;
+  }
+
+  updateTaskData() {
+    console.log(this.editDataId, this.taskName, this.taskStatus);
+
+    this.taskList = this.taskList.map((item) =>
+      item.id === this.editDataId
+        ? { ...item, task: this.taskName, status: this.taskStatus }
+        : item
+    );
+
+    // console.log(this.taskList);
+
+    this.taskName = '';
+    this.taskStatus = '';
+    this.editMode = false;
+  }
 }
